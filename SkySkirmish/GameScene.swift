90d294dd35f2ac903cec8c934ccf455b058e3d51
Noah Rubin <95647477+haonnoah123@users.noreply.github.com>
2022-03-18 10:36:21 -0500

    auto flight path stuff

--- SkySkirmish/GameScene.swift
+++ SkySkirmish/GameScene.swift
@@ -22,11 +22,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var MainWeaponTimer: Timer?
     var WingCannonsTimer: Timer?
     var EnemyTimer: Timer?
+    var RandomTimer: Timer?
     
     var mainWeaponLevel = 0
     let mainWeaponIntervals = [0.425, 0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25, 0.225, 0.2]
     let mainWeaponDamage = [100, 125, 150, 175, 200, 225, 250, 275, 300, 325]
     var cooldown = false
+    var randomPath = 0
+    var enemyIdentifier = 0
     
     enum CollisionType: UInt32 {
         case none = 0
@@ -45,33 +48,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         physicsWorld.contactDelegate = self
         
-        print(UIScreen.main.nativeBounds)
-        let screenWidth = UIScreen.main.nativeBounds.width
-        let screenHeight = UIScreen.main.nativeBounds.height
-        print("The width is \(screenWidth) and the height is \(screenHeight)")
-        print(-screenWidth/2 - 10)
-        //penile
-        
-        let screenWidthTest = UIScreen.main.bounds.width
-        let screenHeightTest = UIScreen.main.bounds.height
-        
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: -screenWidthTest, y: -screenHeightTest))
-        path.addLine(to: CGPoint(x: screenWidthTest, y: screenHeightTest))
-        
-        let path1 = UIBezierPath()
-        path1.move(to: CGPoint(x: -screenWidth/2 - 10, y: (screenHeight/10 - 100)))
-        //path1.addLine(to: CGPoint(x: (screenWidth / 2), y: (screenHeight/2 - 100)))
-        path1.addCurve(to: CGPoint(x: -screenWidth/4, y: -screenHeight/2 - 50), controlPoint1: CGPoint(x: -screenWidth/4, y: screenHeight/20), controlPoint2: CGPoint(x: -screenWidth/4, y: screenHeight/20))
-                      
-                      //, controlPoint1: CGPoint(x: 0, y: 0), controlPoint2: CGPoint(x: 0, y: 0))
-        let path2 = UIBezierPath()
-        path2.move(to: CGPoint(x: screenWidth/2 + 10, y: (screenHeight/10 - 100)))
-        path2.addCurve(to: CGPoint(x: 0, y: -screenHeight/2 - 50), controlPoint1: CGPoint(x: 0, y: screenHeight/20), controlPoint2: CGPoint(x: 0, y: screenHeight/20))
-        
-        let move =  SKAction.follow(path.cgPath, speed: 300)
-        let move1 = SKAction.follow(path1.cgPath, speed: 300)
-        let move2 = SKAction.follow(path2.cgPath, speed: 300)
         
         //initializing player
         player = SKSpriteNode(imageNamed: "small_dot")
@@ -87,25 +63,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(player)
         
         //test boxes for collsion testing
-        var testEnemy: Enemy = Enemy()
-        testEnemy.HP = 1000
-        testBox = SKSpriteNode(imageNamed: "blueRectangle")
-        testBox.name = "Box"
-        testBox.position = CGPoint(x: 100, y: 300)
-        testBox.setScale(0.2)
-        testBox.zPosition = 2
-        testBox.physicsBody = SKPhysicsBody(rectangleOf: testBox.size)
-        testBox.physicsBody?.isDynamic = false
-        testBox.physicsBody?.isResting = true
-        testBox.physicsBody!.affectedByGravity = false
-        testBox.physicsBody?.categoryBitMask = CollisionType.enemy.rawValue
-        testBox.physicsBody?.collisionBitMask = CollisionType.player.rawValue
-        testBox.physicsBody?.contactTestBitMask = CollisionType.player.rawValue
-        testEnemy.enemyNode = testBox
-        enemies.append(testEnemy)
-        self.addChild(testEnemy.enemyNode)
-        
-        testBox.run(move)
+        //testBox.run(move2)
         
         upgrade = SKSpriteNode(imageNamed: "upgradeCircle")
         upgrade.name = "upgrade"
@@ -124,6 +82,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         timerMainWeapon(Level: mainWeaponLevel)
         //timerWingCannons()
         cooldownTimer()
+        enemyTimer()
+        getRandom()
         
         backgroundColor = SKColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
     }
@@ -133,16 +93,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             cooldown = true
             var body1 = SKPhysicsBody()
             var body2 = SKPhysicsBody()
-            print("bruhggg")
             
             if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-                print("bruh")
                 body1 = contact.bodyA
                 body2 = contact.bodyB
             } else {
                 body1 = contact.bodyB
                 body2 = contact.bodyA
-                print("buddy")
             }
             
             if body1.categoryBitMask == CollisionType.player.rawValue && body2.categoryBitMask == CollisionType.enemy.rawValue {
@@ -168,21 +125,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     //explosion
                 }
                 
-                var isFound = false
-                var index = 0
-                while(!isFound){
-                    var count = 0
-                    if(enemies[count].enemyNode.name == body2.node?.name){
-                        enemies[count].HP -= mainWeaponDamage[mainWeaponLevel]
-                        index = count
-                        isFound = true
-                    }
-                    count+=1
-                }
+                let index = findEnemy(body: body2)
+                let tempEnemy = enemies[index]
                 
                 body1.node?.removeFromParent()
                 
-                if(enemies[index].HP <= 0){
+                if(tempEnemy.HP <= 0){
                     body2.node?.removeFromParent()
                     enemies.remove(at: index)
                 }
@@ -198,6 +146,70 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 MainWeaponTimer?.invalidate()
                 MainWeaponTimer = Timer.scheduledTimer(timeInterval: mainWeaponIntervals[mainWeaponLevel], target: self, selector: #selector(self.mainWeapon), userInfo: nil, repeats: true)
             }
+        }
+    }
+    
+    func findEnemy(body: SKPhysicsBody) -> Int {
+        var isFound = false
+        var count = 0
+        while(!isFound){
+            if(enemies[count].enemyNode == body.node!){
+                enemies[count].HP -= mainWeaponDamage[mainWeaponLevel]
+                return count
+                isFound = true
+            }
+            count+=1
+        }
+        return 0
+    }
+    
+    @objc func enemy(){
+        if isPlayerAlive && !gamePaused {
+            let enemyName = "enemy" + String(enemyIdentifier)
+            var testEnemy: Enemy = Enemy()
+            testEnemy.HP = 200
+            testBox = SKSpriteNode(imageNamed: "blueRectangle")
+            testBox.name = enemyName
+            testBox.position = CGPoint(x: 100, y: 300)
+            testBox.setScale(0.2)
+            testBox.zPosition = 2
+            testBox.physicsBody = SKPhysicsBody(rectangleOf: testBox.size)
+            testBox.physicsBody?.isDynamic = false
+            testBox.physicsBody?.isResting = true
+            testBox.physicsBody!.affectedByGravity = false
+            testBox.physicsBody?.categoryBitMask = CollisionType.enemy.rawValue
+            testBox.physicsBody?.collisionBitMask = CollisionType.player.rawValue
+            testBox.physicsBody?.contactTestBitMask = CollisionType.player.rawValue
+            testEnemy.enemyNode = testBox
+            enemies.append(testEnemy)
+            self.addChild(testEnemy.enemyNode)
+            
+            enemyIdentifier += 1
+            
+            let screenWidth = UIScreen.main.bounds.width/2
+            let screenHeight = UIScreen.main.bounds.height/2
+
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: -screenWidth * 2, y: -screenHeight * 2))
+            path.addLine(to: CGPoint(x: screenWidth * 2, y: screenHeight * 2))
+            
+            let path1 = UIBezierPath()
+            path1.move(to: CGPoint(x: -screenWidth, y: screenHeight/2))
+            path1.addCurve(to: CGPoint(x: -screenWidth/2, y: -screenHeight * 3), controlPoint1: CGPoint(x: -screenWidth/3, y: screenHeight/2), controlPoint2: CGPoint(x: -screenWidth/3, y: screenHeight/2))
+            
+            let path2 = UIBezierPath()
+            path2.move(to: CGPoint(x: screenWidth, y: screenHeight/2))
+            path2.addCurve(to: CGPoint(x: screenWidth/2, y: -screenHeight * 3), controlPoint1: CGPoint(x: screenWidth/3, y: screenHeight/2), controlPoint2: CGPoint(x: screenWidth/3, y: screenHeight/2))
+            
+            let move =  SKAction.follow(path.cgPath, speed: 300)
+            let move1 = SKAction.follow(path1.cgPath, speed: 300)
+            let move2 = SKAction.follow(path2.cgPath, speed: 300)
+            
+            let moves: [SKAction] = [move, move1, move2]
+            
+            let deleteObj = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([moves[randomPath], deleteObj])
+            testBox.run(sequence)
         }
     }
     
@@ -270,6 +282,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.run(SKAction.move(to: tempPos, duration: Double(distance) / movementSpeed))
     
     }
+    
+    func getRandom() {
+        RandomTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { Timer in
+            self.randomPath = Int.random(in: 0..<3) })
+    }
+    
     //timers for the player's weapons
     func timerMainWeapon(Level: Int) {
         MainWeaponTimer = Timer.scheduledTimer(timeInterval: mainWeaponIntervals[Level], target: self, selector: #selector(self.mainWeapon), userInfo: nil, repeats: true)
@@ -282,6 +300,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func cooldownTimer() {
         CooldownTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { Timer in
             self.cooldown = false })
+    }
+    
+    func enemyTimer(){
+        EnemyTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.enemy), userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
